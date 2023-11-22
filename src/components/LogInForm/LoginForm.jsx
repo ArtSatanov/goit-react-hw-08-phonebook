61d3bc1d465257427eb4234b4ffdfcbb887fb4a3
Artem Satanovskiy <artsatanov@gmail.com>
2023-11-22 16:04:09 +0200

    1

--- src/components/LogInForm/LoginForm.jsx
+++ src/components/LogInForm/LoginForm.jsx
@@ -3,12 +3,11 @@ import * as Yup from 'yup';
 
 const LoginSchema = Yup.object().shape({
   email: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
+.email('Invalid email')
     .required('Required'),
   password: Yup.string()
     .min(9, 'Too Short!')
-    .max(9, 'Too Long!')
+    .max(15, 'Too Long!')
     .required('Required'),
 });
 
